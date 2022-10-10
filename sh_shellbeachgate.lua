5e018a56d60b743fec476710eae05e4629d36d3e
Mike White <111154202+MikeWhite23@users.noreply.github.com>
2022-10-10 16:52:10 +0200

    Reworked into 1 command

--- sh_shellbeachgate.lua
+++ sh_shellbeachgate.lua
@@ -1,36 +1,29 @@
 if string.find(game.GetMap(), "rp_apex_industrial") == nil then return end
 
-local sbgateson = {
-	description = "Opens Shell Beach Gates (DvL+)",
+local IsOpened = false
+
+local sbgatecmd = {
+	description = "Toggle Shell Beach Gates (DvL+)",
 	requiesAlive = true,
 	onRun = function(ply, arg, rawText)
 		if not ply:IsCPCommand() then
 			ply:Notify("You are not authorized to use this command.")
 			return
 		end
-
-		for v,k in pairs(ents.FindByName("nexus_globe_bulkhead_open_button")) do
-			k:Fire("Use")
-		end
-		ply:Notify("You have opened shell beach gate.")
+		if IsOpened == false then
+			for v,k in pairs(ents.FindByName("nexus_globe_bulkhead_open_button")) do
+				k:Fire("Use")
+			end
+			ply:Notify("You have opened shell beach gate.")
+			IsOpened = true
+		elseif IsOpened == true then
+			for v,k in pairs(ents.FindByName("nexus_globe_bulkhead_close_button")) do
+				k:Fire("Use")
+			end
+			ply:Notify("You have closed shell beach gate.")
+			IsOpened = false
+		end	
 	end
 }
 
-local sbgatesoff = {
-	description = "Closes Shell Beach Gates (DvL+)",
-	requiesAlive = true,
-	onRun = function(ply, arg, rawText)
-		if not ply:IsCPCommand() then
-			ply:Notify("You are not authorized to use this command.")
-			return
-		end
-
-		for v,k in pairs(ents.FindByName("nexus_globe_bulkhead_close_button")) do
-			k:Fire("Use")
-		end
-		ply:Notify("You have closed shell beach gate.")
-	end
-}
-
-impulse.RegisterChatCommand("/sbclose", sbgatesoff)
-impulse.RegisterChatCommand("/sbopen", sbgateson)
+impulse.RegisterChatCommand("/togglesbgate", sbgatecmd)
